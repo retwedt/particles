eee1c5334a125509493e0fcd51b628669d5f28ee
retwedt <retwedt@gmail.com>
2016-01-26 15:42:46 -0600

    initial commit for particle burst demo

--- js/particle.js
+++ js/particle.js
@@ -1,28 +1,16 @@
 /*
 	Define all variables and methods needed for our custom particles
 
-	Use a constructor function to hold variables
-		Variable values will be unique for each instance, 
-		so don't use prototype!
-
-	Use prototype to create methods that will be inherited
-	by all instances of the Particle object
-		These methods will not change with each instance,
-		so you can use prototype!
-
 */ 
 
 
 // Particle constructor - defines the Particle object and any of its variables 
-function Particle (xCenter, yCenter, radius, angle, speed, colorObj) {
+function Particle (xCenter, yCenter, radius, xVelocity, yVelocity, colorObj, life, mass) {
 	this.x = xCenter;
 	this.y = yCenter;
 	this.radius = radius;
-
-	this.angle = angle;
-	this.speed = speed;
-	this.xVelocity = this.speed*cos(this.angle); // Horizontal velocity
-	this.yVelocity = this.speed*sin(this.angle); // Vertical velocity
+	this.xVelocity = xVelocity; // Horizontal velocity
+	this.yVelocity = yVelocity; // Vertical velocity
 
 	// this.color = color([colorObj.r, colorObj.g, colorObj.b, colorObj.a]);
 	this.color = {
@@ -32,48 +20,53 @@ function Particle (xCenter, yCenter, radius, angle, speed, colorObj) {
 		a:colorObj.a
 	};
 
+	this.life = life;
+	this.mass = mass;
+
 	this.isColliding = false;
 }
 
 
 // Update position of each particle and check for wall collisions
-Particle.prototype.update = function () {
+Particle.prototype.update = function (gravity) {
+	this.life--; // life of particle goes down each frame
+
 	// Update particle position using velocity
-	this.x += this.xVelocity;
-	this.y += this.yVelocity;
+	this.x += this.mass*this.xVelocity;
+	this.yVelocity += 1;
+	this.y += this.mass*this.yVelocity;
 
 	// Left wall
 	if (this.x < this.radius) { 
 		this.x = this.radius;
-		this.xVelocity *= -1;
+		// this.xVelocity *= -1;
+		this.xVelocity *= -0.9; // Friction
 	} 
 	// Right wall
 	else if (this.x > (width - this.radius)) { 
 		this.x = width - this.radius;
-		this.xVelocity *= -1;
+		// this.xVelocity *= -1;
+		this.xVelocity *= -0.9; // Friction
 	}
-
 	// Top wall
 	if (this.y < this.radius) { 
 		this.y = this.radius;
-		this.yVelocity *= -1;
+		// this.yVelocity *= -1;
+		this.yVelocity *= -0.9; // Friction
 	}
 	// Bottom wall 
 	else if (this.y > (height - this.radius)) { 
 		this.y = height - this.radius;
-		this.yVelocity *= -1;
+		// this.yVelocity *= -1;
+		this.yVelocity *= -0.9; // Friction
 	}
-
-	this.angle = atan2(this.yVelocity, this.xVelocity);
-	this.speed = sqrt((this.xVelocity*this.xVelocity) + (this.yVelocity + this.yVelocity));
-
-
 };
 
 
 // Draw the particle
 Particle.prototype.draw = function () {
 	fill(this.color.r, this.color.g, this.color.b, this.color.a);
+	// fill(this.color);
 	if (this.isColliding){
 		strokeWeight(1);
 		stroke('white');
@@ -81,20 +74,4 @@ Particle.prototype.draw = function () {
 		noStroke();
 	}
 	ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
-
-	noFill();
-	stroke("white");
-
-	var lineX = this.radius*cos(this.angle);
-	var lineY = this.radius*sin(this.angle);
-	line(this.x, this.y, (this.x+lineX), (this.y+lineY));
-
-	
-	var slope = lineY/lineX;
-	var tanSlope = -lineX/lineY;
-
-	stroke("blue");
-
-	line((this.x+lineX)-(lineY), (this.y+lineY)+(lineX), (this.x+lineX)+(lineY), (this.y+lineY)-(lineX));
-
 };
--- js/sketch.js
+++ js/sketch.js
@@ -1,66 +1,56 @@
 /*
-	Particles using Objects!
-
-	TOPICS TO COVER IN DEMO
-	=======================
-	WHAT IS A CONSTRUCTOR?
-	WHAT IS PROTOTYPE?
-	WHAT IS THIS?
-	DEFAULT VALUES FOR CONSTRUCTORS?
-
-	Concepts to review:
-		Arrays
-		Array.push()
-		ellipse()
+	Particle Burst!
 
 */ 
 
-var ball; // Global variable for a single particle
-var ballArray = []; // Global array variable to hold balls..........
-var ballPit; // Global variable to hold particle group object
+
+var allParticles = []; // Array to hold all particle groups on screen
+var gravity = 0.6; // Global variable for gravity
+
 
 function setup(){
 	createCanvas(windowWidth, windowHeight); // Create canvas that fills screen
 
-	// // Create a single ball using the particle object!
-	// var redColor = random(0, 255);
-	// var radius = random(50, 100);
-	// var xCenter = random(radius, width - radius);
-	// var yCenter = random(radius, height - radius);
-	// var xVelocity = random(-10, 10);
-	// var yVelocity = random(-10, 10);
-	// ball = new Particle(xCenter, yCenter, radius, xVelocity, yVelocity, redColor);
-
-	// // Fill an array with balls!
-	// for (var i=0; i<20; i++){
-	// 	var redColor = random(0, 255);
-	// 	var radius = random(50, 100);
-	// 	var xCenter = random(radius, width - radius);
-	// 	var yCenter = random(radius, height - radius);
-	// 	var xVelocity = random(-10, 10);
-	// 	var yVelocity = random(-10, 10);
-	// 	ball = new Particle(xCenter, yCenter, radius, xVelocity, yVelocity, redColor);
-
-	// 	ballArray.push(ball);
-	// }
-
-	// Create a new particle group to hold the ball pit
-	ballPit = new ParticleGroup(20);
 }
 
+
 function draw() {
-	background(255); // Fill background with white
+	fill(255, 255, 255, 30);
+	rect(0,0, width, height);
 
-	// // Draw a single ball!
-	// ball.draw();
-	// ball.update();
+	noStroke();
 
-	// // Draw array of balls!
-	// for (var i=0; i<ballArray.length; i++){
-	// 	ballArray[i].draw();
-	// 	ballArray[i].update();
-	// }
+	// If a particle group has particles in it, draw the particles and update their position
+	// If the particle group is empty, remove it from the allparticles array
+	for (var i=0; i<allParticles.length; i++){
+		if (allParticles[i].particles.length > 0){
+			allParticles[i].draw();
+			allParticles[i].update(gravity);
+		} else {
+			allParticles.splice(i, 1);
+			i--;
+		}
+	}
+}
 
-	ballPit.draw();
-	ballPit.update();
+
+function mousePressed(){
+	var burst = new ParticleGroup(random(60, 80), mouseX, mouseY);
+	allParticles.push(burst);
+}
+
+
+function touchStarted(){
+	var burst = new ParticleGroup(random(60, 80), touchX, touchY);
+	allParticles.push(burst);
+}
+
+
+function touchMoved(){
+	return false;
+}
+
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
 }
