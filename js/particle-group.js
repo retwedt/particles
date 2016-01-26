9d207e98e1c3bae2fb52846fe47eb1628b4bd693
retwedt <retwedt@gmail.com>
2016-01-26 15:56:47 -0600

    add particle burst demo to gh-pages branch
    for live demos

--- js/particle-group.js
+++ js/particle-group.js
@@ -6,26 +6,35 @@
 
 // ParticleGroup constructor - defines the ParticleGroup and any of it's 
 // variables
-function ParticleGroup(size) {
+function ParticleGroup(size, x, y) {
 	this.particles = [];
-	this.colX = 0;
-	this.colY = 0;
 
 	// Create the number of particles specified and store them in the group
 	for (var i = 0; i < size; i++) {
 		// Random values for particle parameters
-		var radius = random(80, 120);
-		var angle = random(0, 360);
-		var speed = random(8, 10);
-		var x = random(radius, width-radius);
-		var y = random(radius, height-radius);
+		var radius = random(20, 60);
+		var xVelocity = random(-12, 12);
+		var yVelocity = random(-12, 12);
+		if (xVelocity > 0){ // Add min x values
+			xVelocity += random(3, 5);
+		} else {
+			xVelocity -= random(3, 5);
+		}
+		if (yVelocity > 0){ // Add min y values
+			yVelocity += random(3, 5);
+		} else {
+			yVelocity -= random(3, 5);
+		}
+		// var particleColor = [random(0, 255), 0, 0, 100];
 		var particleColor = {
 			r: random(0, 255),
 			g: 0,
 			b: 0,
 			a:200
 		};
-		var particle = new Particle(x, y, radius, angle, speed, particleColor);
+		var life = random(40, 200);
+		var mass = random(1, 2);
+		var particle = new Particle(x, y, radius, xVelocity, yVelocity, particleColor, life, mass);
 
 		this.particles.push(particle);
 	}
@@ -33,10 +42,12 @@ function ParticleGroup(size) {
 
 
 // Update all particles in the group
-ParticleGroup.prototype.update = function () {
+ParticleGroup.prototype.update = function (gravity) {
 	for (var i = 0; i < this.particles.length; i++) {
-		this.particles[i].update();
+		// Call the particle's update function, pass gravity as a parameter
+		this.particles[i].update(gravity);
 	}
+	this.isAlive();
 	this.checkAllCollisions();
 };
 
@@ -45,49 +56,26 @@ ParticleGroup.prototype.update = function () {
 ParticleGroup.prototype.draw = function () {
 	for (var i = 0; i < this.particles.length; i++) {
 		// Call the particle's draw function
+		if (this.particles[i].life>=0){
 			this.particles[i].draw();
+		}
 	}
 };
 
 
 ParticleGroup.prototype.checkCollision = function (ball1, ball2) {
 	var distance = dist(ball1.x, ball1.y, ball2.x, ball2.y);
-
-	// labels
-	text("1", ball1.x, ball1.y);
-	stroke("green");
-	line(ball1.x, ball1.y, ball2.x, ball2.y);
-
-
-	// calculate angle of intersection between two particles
-	var slope = (ball2.y - ball1.y)/(ball2.x - ball1.x);
-	var tanSlope =  -(ball2.x - ball1.x)/(ball2.y - ball1.y);
-	var intersectionAngle = atan2((ball2.y - ball1.y), (ball2.x - ball1.x));
-	var lineX = ball1.radius*cos(intersectionAngle);
-	var lineY = ball1.radius*sin(intersectionAngle);
-
-	// if particles are colliding, display the intersection point and a
-	// the collision plane
-	if (distance <= ball1.radius + ball2.radius) {
-
-		fill("black");
-		stroke("black");
-		ellipse((ball1.x + lineX), (ball1.y + lineY), 10, 10);
-		line((ball1.x+lineX)-(lineY), (ball1.y+lineY)+(lineX), (ball1.x+lineX)+(lineY), (ball1.y+lineY)-(lineX));
-
-		// if particles are colliding, return true
+	if (distance <= ball1.radius + ball2.radius && ball1.life>=0 && ball2.life>=0) {
 		return true;
 	}
-	// if particles are not colliding, return false
 	return false;
 };
 
 
 ParticleGroup.prototype.checkAllCollisions = function() {
-
-	// reset particles to default state (not green, not colliding)
 	for (var i=0; i<this.particles.length; i++){
-		this.particles[i].color.g = 0;
+		// this.particles[i].color.g = 0;
+		// this.particles[i].color[1] = 0;
 		this.particles[i].isColliding = false;
 	}
 
@@ -95,16 +83,33 @@ ParticleGroup.prototype.checkAllCollisions = function() {
 		for (var j=i+1; j<this.particles.length; j++){
 			var ball1 = this.particles[i];
 			var ball2 = this.particles[j];
-			// if there is a collision between two particles, 
 			if (this.checkCollision(ball1, ball2)) {
-	// 			this.colX = ball1.radius*cos(ball1.angle);
-	// 			this.colY = ball1.radius*sin(ball1.angle);
-	// ellipse(this.particles[i].x+this.colX, this.particles[i].y+this.colY, 5, 5);
 				ball1.isColliding = true;
 				ball2.isColliding = true;
-				ball1.color.g = 50;
-				ball2.color.g = 50;
+				// ball1.color.g = 255;
+				// ball2.color.g = 255;
+				// ball1.color[1] = 255;
+				// ball2.color[1] = 255;
 			}
+		}
+	}
+};
+
+
+// Check if any particles are alive in the group
+// If a particle's life is 0 or less, remove it from the particles array
+ParticleGroup.prototype.isAlive = function(){
+	for (var i=0; i<this.particles.length; i++){
+		if (this.particles[i].life<= 40){
+			// var x = 100;
+			// var a = map(x, 0, this.particles[i].color.a, 0, 60);
+			// this.particles[i].color.a = a;
+			this.particles[i].color.a -= 5;
+		}
+
+		if (this.particles[i].life<=0){
+			this.particles.splice(i, 1);
+			i--;
 		}
 	}
 };
